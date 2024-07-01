c96bb7afcb1345b3f63d4f7288a0c76f1a818ae4
JustReddy <novoiceover2@gmail.com>
2024-07-01 12:51:51 +0200

    Added GameMaps

---
+++ api/src/main/java/ga/justreddy/wiki/whaleskywars/api/model/game/map/IGameMap.java
@@ -0,0 +1,19 @@
+package ga.justreddy.wiki.whaleskywars.api.model.game.map;
+
+import ga.justreddy.wiki.whaleskywars.api.model.game.IGame;
+import org.bukkit.World;
+
+/**
+ * @author JustReddy
+ */
+public interface IGameMap {
+
+    void onEnable(IGame game);
+
+    void onRestart(IGame game);
+
+    void onDisable(IGame game);
+
+    World getBukkitWorld();
+
+}
---
+++ plugin/src/main/java/ga/justreddy/wiki/whaleskywars/model/game/map/SlimeGameMap.java
@@ -0,0 +1,31 @@
+package ga.justreddy.wiki.whaleskywars.model.game.map;
+
+import ga.justreddy.wiki.whaleskywars.api.model.game.IGame;
+import ga.justreddy.wiki.whaleskywars.api.model.game.map.IGameMap;
+import org.bukkit.World;
+
+/**
+ * @author JustReddy
+ */
+public class SlimeGameMap implements IGameMap {
+
+    @Override
+    public void onEnable(IGame game) {
+
+    }
+
+    @Override
+    public void onRestart(IGame game) {
+
+    }
+
+    @Override
+    public void onDisable(IGame game) {
+
+    }
+
+    @Override
+    public World getBukkitWorld() {
+        return null;
+    }
+}
