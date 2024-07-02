9696152c15618c5a22caef23763c54114f9c41ff
JustReddy <novoiceover2@gmail.com>
2024-07-02 14:54:34 +0200

    NMS stuff

--- plugin/src/main/java/ga/justreddy/wiki/whaleskywars/version/nms/INms.java
+++ plugin/src/main/java/ga/justreddy/wiki/whaleskywars/version/nms/INms.java
@@ -1,7 +1,12 @@
 package ga.justreddy.wiki.whaleskywars.version.nms;
 
+import org.bukkit.generator.ChunkGenerator;
+
 /**
  * @author JustReddy
  */
 public interface INms {
+
+    ChunkGenerator getChunkGenerator();
+
 }
--- v1_8_R3/src/main/java/ga/justreddy/wiki/v1_8_R3/v1_8_R3.java
+++ v1_8_R3/src/main/java/ga/justreddy/wiki/v1_8_R3/v1_8_R3.java
@@ -1,7 +1,12 @@
 package ga.justreddy.wiki.v1_8_R3;
 
 import ga.justreddy.wiki.whaleskywars.version.nms.INms;
+import org.bukkit.generator.ChunkGenerator;
 
 public final class v1_8_R3 implements INms {
 
+    @Override
+    public ChunkGenerator getChunkGenerator() {
+        return null;
+    }
 }
