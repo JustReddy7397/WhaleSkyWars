5463d0703a130d63d7c9349575fc38b3bed38af1
JustReddy7397 <novoiceover2@gmail.com>
2025-08-26 12:20:46 +0200

    Update GameSign.java

--- plugin/src/main/java/ga/justreddy/wiki/whaleskywars/model/game/GameSign.java
+++ plugin/src/main/java/ga/justreddy/wiki/whaleskywars/model/game/GameSign.java
@@ -14,12 +14,15 @@ import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
 
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 /**
  * @author JustReddy
  */
 public class GameSign implements IGameSign {
+
+    private static final Logger LOGGER = WhaleSkyWars.getInstance().getLogger();
 
     private final String id;
     private final Location location;
@@ -41,136 +44,115 @@ public class GameSign implements IGameSign {
 
     @Override
     public void update(IGame game) {
-        if (location.getBlock() == null) return;
-        BlockState blockState = location.getBlock().getState();
-        if (blockState == null) return;
+        Block block = location.getBlock();
+        if (block == null) return;
+        BlockState blockState = block.getState();
         if (!(blockState instanceof Sign)) return;
         Sign sign = (Sign) blockState;
+
         TomlConfig config = WhaleSkyWars.getInstance().getSignsConfig();
         GameState state = game.getGameState();
-        String gameStateLine = "";
-        String materialName = "BLACK_TERRACOTTA";
-        switch (state) {
-            case WAITING:
-                gameStateLine = config.getString("states.waiting");
-                materialName = config.getString("blocks.waiting");
-                break;
-            case STARTING:
-                gameStateLine = config.getString("states.starting");
-                materialName = config.getString("blocks.starting");
-                break;
-            case PREGAME:
-            case PLAYING:
-                gameStateLine = config.getString("states.playing");
-                materialName = config.getString("blocks.playing");
-                break;
-            case ENDING:
-                gameStateLine = config.getString("states.ending");
-                materialName = config.getString("blocks.ending");
-                break;
-            case RESTORING:
-                gameStateLine = config.getString("states.restarting");
-                materialName = config.getString("blocks.restarting");
-                break;
-            case DISABLED:
-                gameStateLine = config.getString("states.disabled");
-                materialName = config.getString("blocks.disabled");
-                break;
-        }
-        String finalGameStateLine = gameStateLine;
-        List<String> lines = config.getStringList("lines")
-                .stream()
-                .map(line -> TextUtil.color(line
-                        .replaceAll("<map>", game.getDisplayName())
-                        .replaceAll("<state>", finalGameStateLine)
-                        .replaceAll("<players>", String.valueOf(game.getPlayers().size()))
-                        .replaceAll("<max-players>", String.valueOf(game.getMaximumPlayers()))
-                        .replaceAll("<mode>", game.getGameMode().getIdentifier()))).collect(Collectors.toList());
+        String gameStateLine = getGameStateLine(config, state);
+        String materialName = getMaterialName(config, state);
 
-        for (int i = 0; i < lines.size(); i++) {
+        List<String> lines = config.getStringList("lines").stream()
+                .map(line -> TextUtil.color(line
+                        .replace("<map>", game.getDisplayName())
+                        .replace("<state>", gameStateLine)
+                        .replace("<players>", String.valueOf(game.getPlayers().size()))
+                        .replace("<max-players>", String.valueOf(game.getMaximumPlayers()))
+                        .replace("<mode>", game.getGameMode().getIdentifier())))
+                .collect(Collectors.toList());
+
+        int signLines = sign.getLines().length;
+        for (int i = 0; i < lines.size() && i < signLines; i++) {
             sign.setLine(i, lines.get(i));
         }
-
         sign.update(true);
 
-        String finalMaterialName = materialName;
         XMaterial.matchXMaterial(materialName)
-                .ifPresentOrElse(mat -> {
-                    XBlock.setType(getRelativeBlock(), mat);
-                }, () -> {
-                    WhaleSkyWars.getInstance().getLogger().warning("Invalid material name: " + finalMaterialName);
-                });
-
+                .ifPresent(mat -> XBlock.setType(getRelativeBlock(), mat));
     }
 
     @Override
     public void updateBungee(Object bungeeGame) {
         if (!(bungeeGame instanceof BungeeGame)) return;
         BungeeGame game = (BungeeGame) bungeeGame;
-        if (location.getBlock() == null) return;
-        BlockState blockState = location.getBlock().getState();
-        if (blockState == null) return;
+
+        Block block = location.getBlock();
+        if (block == null) return;
+        BlockState blockState = block.getState();
         if (!(blockState instanceof Sign)) return;
         Sign sign = (Sign) blockState;
+
         TomlConfig config = WhaleSkyWars.getInstance().getSignsConfig();
         GameState state = game.getGameState();
-        String gameStateLine = "";
-        String materialName = "BLACK_TERRACOTTA";
-        switch (state) {
-            case WAITING:
-                gameStateLine = config.getString("states.waiting");
-                materialName = config.getString("blocks.waiting");
-                break;
-            case STARTING:
-                gameStateLine = config.getString("states.starting");
-                materialName = config.getString("blocks.starting");
-                break;
-            case PREGAME:
-            case PLAYING:
-                gameStateLine = config.getString("states.playing");
-                materialName = config.getString("blocks.playing");
-                break;
-            case ENDING:
-                gameStateLine = config.getString("states.ending");
-                materialName = config.getString("blocks.ending");
-                break;
-            case RESTORING:
-                gameStateLine = config.getString("states.restarting");
-                materialName = config.getString("blocks.restarting");
-                break;
-            case DISABLED:
-                gameStateLine = config.getString("states.disabled");
-                materialName = config.getString("blocks.disabled");
-                break;
-        }
-        String finalGameStateLine = gameStateLine;
-        List<String> lines = config.getStringList("lines")
-                .stream()
-                .map(line -> {
-                    return line
-                            .replaceAll("<map>", game.getName())
-                            .replaceAll("<state>", finalGameStateLine)
-                            .replaceAll("<players>", String.valueOf(game.getPlayers().size()))
-                            .replaceAll("<max-players>", String.valueOf(game.getMaxPlayers()))
-                            .replaceAll("<mode>", game.getGameMode().getIdentifier());
-                }).collect(Collectors.toList());
-        for (int i = 0; i < lines.size(); i++) {
+        String gameStateLine = getGameStateLine(config, state);
+        String materialName = getMaterialName(config, state);
+
+        List<String> lines = config.getStringList("lines").stream()
+                .map(line -> TextUtil.color(line
+                        .replace("<map>", game.getName())
+                        .replace("<state>", gameStateLine)
+                        .replace("<players>", String.valueOf(game.getPlayers().size()))
+                        .replace("<max-players>", String.valueOf(game.getMaxPlayers()))
+                        .replace("<mode>", game.getGameMode().getIdentifier())))
+                .collect(Collectors.toList());
+
+        int signLines = sign.getLines().length;
+        for (int i = 0; i < lines.size() && i < signLines; i++) {
             sign.setLine(i, lines.get(i));
         }
-
         sign.update(true);
 
-        String finalMaterialName = materialName;
         XMaterial.matchXMaterial(materialName)
-                .ifPresentOrElse(mat -> {
-                    XBlock.setType(getRelativeBlock(), mat);
-                }, () -> {
-                    WhaleSkyWars.getInstance().getLogger().warning("Invalid material name: " + finalMaterialName);
-                });
+                .ifPresent(mat -> XBlock.setType(getRelativeBlock(), mat));
     }
 
     @Override
     public Block getRelativeBlock() {
-        return WhaleSkyWars.getInstance().getNms().getRelativeBlock(getLocation());
+        return WhaleSkyWars.getInstance().getNms().getRelativeBlock(location);
+    }
+
+    private String getGameStateLine(TomlConfig config, GameState state) {
+        if (state == null) return "";
+        switch (state) {
+            case WAITING:
+                return config.getString("states.waiting");
+            case STARTING:
+                return config.getString("states.starting");
+            case PREGAME:
+            case PLAYING:
+                return config.getString("states.playing");
+            case ENDING:
+                return config.getString("states.ending");
+            case RESTORING:
+                return config.getString("states.restarting");
+            case DISABLED:
+                return config.getString("states.disabled");
+            default:
+                return "";
+        }
+    }
+
+    private String getMaterialName(TomlConfig config, GameState state) {
+        if (state == null) return "BLACK_TERRACOTTA";
+        switch (state) {
+            case WAITING:
+                return config.getString("blocks.waiting");
+            case STARTING:
+                return config.getString("blocks.starting");
+            case PREGAME:
+            case PLAYING:
+                return config.getString("blocks.playing");
+            case ENDING:
+                return config.getString("blocks.ending");
+            case RESTORING:
+                return config.getString("blocks.restarting");
+            case DISABLED:
+                return config.getString("blocks.disabled");
+            default:
+                return "BLACK_TERRACOTTA";
+        }
     }
 }
